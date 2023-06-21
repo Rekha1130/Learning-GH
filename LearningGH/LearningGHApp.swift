22c08b9a6be0415622b46f740b2dde19a32e82ad
Rekha <erina.rekha@icloud.com>
2023-06-20 17:51:09 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Rekha Aldas on 20/06/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
